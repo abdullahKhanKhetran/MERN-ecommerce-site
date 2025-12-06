9dfcb5ff5f8ea7a143321f71798e1fc7f5dc7777
abdullahKhanKhetran <abdullahkhitran2005@gmail.com>
2025-12-06 14:53:09 +0500

    restored project

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from './ammar.webp';
+import logo2 from './kala.webp';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo2} className="App-logo" alt="logo" />
+        <p>
+          zeeshan Gando
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/abdullahKhanKhetran"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
